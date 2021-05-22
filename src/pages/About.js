8b1d970842028a7f3ec2b49da4488c3c4853368c
Vivek Mohan <vivekmohan27@gmail.com>
2021-05-22 13:03:41 +0530

    feat: made basic

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section class="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat id,
+        officia quidem ullam corrupti ad dolor natus ipsam perferendis. Deserunt
+        porro sequi, natus neque explicabo ipsum quod! Tempore, unde animi?
+      </p>
+    </section>
+  );
+}
